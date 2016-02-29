15f2637b1bd471a52999d410111462080e43241d
ironbob <422049073@qq.com>
2016-02-29 19:35:33 +0800

    fixed

--- app.js
+++ app.js
@@ -3,8 +3,19 @@ var request = require('request');
 var cors=require('cors');
 var app = express();  
 // var apiServerHost='http://121.197.2.240:8080';
-var apiServerHost='http://192.168.64.31:9101';
+var apiServerHost='http://172.16.64.31:9102';
 app.use(cors());
+
+app.use(function(req, res) {  
+	console.log("新请求-----------------------------时间："+new Date().toString());
+	console.log("path:"+req.path);
+	console.log("url:"+req.url);
+	console.log("method:"+req.method);
+	console.log("body:"+JSON.stringify(req.body));
+
+  var url = apiServerHost + req.url;
+  req.pipe(request(url)).pipe(res);
+});
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
@@ -35,17 +46,6 @@ app.use(function(err, req, res, next) {
         error: {}
     });
 });
-app.use(function(req, res) {  
-	console.log("新请求-----------------------------时间："+new Date().toString());
-	console.log("path:"+req.path);
-	console.log("url:"+req.url);
-	console.log("method:"+req.method);
-	console.log("body:"+JSON.stringify(req.body));
-
-  var url = apiServerHost + req.url;
-  req.pipe(request(url)).pipe(res);
-});
-
 
 var port=process.env.PORT || 8989;
 console.log("在"+port+'上开始监听...');
