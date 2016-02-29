519ffc48d4378c311a7a387c28cae8b12ed2ac29
ironbob <422049073@qq.com>
2016-02-29 15:54:42 +0800

    第一次提交

---
+++ app.js
@@ -0,0 +1,52 @@
+var express = require('express');
+var request = require('request');
+var cors=require('cors');
+var app = express();  
+// var apiServerHost='http://121.197.2.240:8080';
+var apiServerHost='http://192.168.64.31:9101';
+app.use(cors());
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+app.use(function(req, res) {  
+	console.log("新请求-----------------------------时间："+new Date().toString());
+	console.log("path:"+req.path);
+	console.log("url:"+req.url);
+	console.log("method:"+req.method);
+	console.log("body:"+JSON.stringify(req.body));
+
+  var url = apiServerHost + req.url;
+  req.pipe(request(url)).pipe(res);
+});
+
+
+var port=process.env.PORT || 8989;
+console.log("在"+port+'上开始监听...');
+app.listen(port); 
